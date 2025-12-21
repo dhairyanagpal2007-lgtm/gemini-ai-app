080e10eeda1feedea962e35b8efd9fe84691645b
Dhairya <your-email@gmail.com>
2025-12-21 10:17:56 +0530

    Update index.html and web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 app = Flask(__name__)
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("API_KEY"))
 model = genai.GenerativeModel("models/gemini-2.5-flash")
 
 @app.route("/", methods=["GET", "POST"])
